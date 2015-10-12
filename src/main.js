090a04db2e9f04f1399ba72b52f08fa408ee2ab0
Ryan Muller <rkennedy@kennship.com>
2015-10-11 21:05:38 -0400

    Initial commit on this branch

--- src/main.js
+++ src/main.js
@@ -1,186 +1,33 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import moment from 'moment';
+$(function () {
+  var PHOTO_INTERVAL = 5000;
+  var PHOTO_ANGLE_VARIANCE = 30;
+  var photoList = null;
+  var $leftPanel = $('.left-panel');
+  var interval = null;
 
-const PHOTO_ANGLE_VARIANCE = 30;
-const PHOTO_INTERVAL = 5e3;
-const FRAMERATE = 24;
-const SLIDE_SPEED = 60e-2;
-const PHYSICS_COEFF = 0.15;
-
-let leftPanel = {
-  width: $('.left-panel').width(),
-  height: $('.left-panel').height()
-};
-
-const FRAME_DUR = 1e3/FRAMERATE;
-
-class Polaroid extends Component {
-  render() {
-    return (<div className="polaroid">
-      <img src={this.props.src}/>
-    </div>);
-  }
-}
-
-class MenuItem extends Component {
-  render() {
-    return (<a className="item" href={this.props.href}>{this.props.name}</a>);
-  }
-}
-
-let n = 0;
-
-function makePhoto(src) {
-  const targetAngle = Math.random() * PHOTO_ANGLE_VARIANCE - PHOTO_ANGLE_VARIANCE / 2;
-  return {
-    targetX: (0.4 + Math.random() * 0.5) * leftPanel.width,
-    targetY: (Math.random() * 0.1) * leftPanel.height,
-    targetAngle,
-    x: 1200,
-    y: (Math.random() * 0.5) * leftPanel.height,
-    angle: targetAngle + Math.random() * PHOTO_ANGLE_VARIANCE * 10 - PHOTO_ANGLE_VARIANCE * 5,
-    createdAt: +new Date(),
-    n: n++,
-    src
-  };
-}
-
-function linterp(x_current, x_target) {
-  return (1 - PHYSICS_COEFF) * x_current + PHYSICS_COEFF * x_target;
-}
-
-function tick(photo) {
-  const {x,y,angle,targetX,targetY,targetAngle} = photo;
-  return {
-    ...photo,
-    x: linterp(x, targetX),
-    y: linterp(y, targetY),
-    angle: linterp(angle, targetAngle),
-    targetY: targetY + SLIDE_SPEED
-  };
-}
-
-function isAlive(photo) {
-  return ((+new Date) - photo.createdAt) < 60e3;
-}
-
-class LeftPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: []
-    };
-    this.fpsInterval = setInterval(this.tick.bind(this), FRAME_DUR);
-  }
-  componentDidMount() {
-    $.get('/images.json').then((sources) => {
-      this.setState({sources});
-      console.log(this.state);
-      this.interval = setInterval(this.addPhoto.bind(this), PHOTO_INTERVAL);
-      this.addPhoto();
+  function tossPhoto() {
+    var src = photoList[Math.floor(Math.random() * photoList.length)];
+    var $img = $('<img>').attr('src',src);
+    var $photo = $('<div>').addClass('polaroid');
+    var rotation = (Math.random() * PHOTO_ANGLE_VARIANCE - PHOTO_ANGLE_VARIANCE/2)+'deg';
+    var x = Math.random() * $leftPanel.width()/2 + $leftPanel.width() / 2;
+    var y = $leftPanel.height() * .25 + Math.random() * $leftPanel.height() * .5;
+    $photo.append($img);
+    $photo.css({
+      transform: 'rotate('+rotation+') translate(-50%,-50%)',
+      top: y,
+      left: x
     });
-  }
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.interval = null;
+    $leftPanel.append($photo);
+    if ($('.polaroid').length >= 10) {
+      clearInterval(interval);
+      interval = null;
     }
   }
-  tick() {
-    this.setState({
-      photos: this.state.photos.map(tick)
-    });
-  }
-  addPhoto() {
-    const {sources} = this.state;
-    const photo = makePhoto(sources[Math.floor(Math.random()*sources.length)]);
-    this.setState({
-      photos: this.state.photos.filter(isAlive).concat([photo])
-    });
-    console.log('added photo', photo);
-  }
-  renderMenuItems() {
-    return ([
-      "Welcome",
-      "About us",
-      "Proposal",
-      "Ceremony",
-      "Reception",
-      "Wedding party",
-      "Registry",
-      "The date"
-    ]).map((name,i) => (<MenuItem key={i} name={name}/>));
-  }
-  renderPhotos() {
-    return this.state.photos.map(photo => (<div className="polaroid" style={{
-      top: 0,
-      left: 0,
-      transform: `translate(${photo.x}px, ${photo.y}px) rotate(${photo.angle}deg) translate(-50%,-50%)`
-    }} key={photo.n}>
-      <img src={photo.src}/>
-    </div>));
-  }
-  render() {
-    return (<div>
-      <h1 className="names">
-        <a href="#">
-          <span>Grace</span>
-          <span>and</span>
-          <span>Ryan</span>
-        </a>
-      </h1>
-      {this.renderPhotos()}
-      <div className="huge inverted secondary vertical ui menu" style={{
-        zIndex: 1002
-      }}>
-        {this.renderMenuItems()}
-      </div>
-    </div>);
-  }
-}
 
-class CountdownHeader extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      status: 'June 25, 2016'
-    };
-    this.fpsInterval = setInterval(this.tick.bind(this), 1e3);
-    this.date = (new Date(2016, 6, 25, 11, 0, 0));
-  }
-  componentDidMount() {
-    this.tick();
-  }
-  tick() {
-    const now = (new Date);
-    let diff = this.date - now;
-    const seconds = Math.floor((diff /= 1e3)%1e3);
-    const minutes = Math.floor((diff /= 60)%60);
-    const hours = Math.floor((diff /= 60)%60);
-    const days = Math.floor((diff/24)%24);
-    this.setState({
-      status: `${days} ${hours}:${minutes}:${seconds}`
-    });
-  }
-  render() {
-    return (<span>{this.state.status}</span>);
-  }
-}
-
-ReactDOM.render(<LeftPanel/>, $('.left-panel')[0]);
-ReactDOM.render(<CountdownHeader/>, $('.wedding-countdown')[0]);
-
-
-$(() => {
-  const $leftPanel = $('.left-panel');
-
-  function updateDims() {
-    leftPanel = {
-      width: $leftPanel.width(),
-      height: $leftPanel.height()
-    };
-  }
-  $(window).on('resize', updateDims);
-  updateDims();
+  $.get('/images.json').then(function (data) {
+    photoList = data;
+    tossPhoto();
+    interval = setInterval(tossPhoto, PHOTO_INTERVAL);
+  });
 });
