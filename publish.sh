f778aab0489b36e02788cdbacfc02bf50714d844
Ryan Muller <rkennedy@kennship.com>
2015-10-23 18:56:11 -0400

    So apparently ZSH shortcuts don't work in bash, who knew? Sheesh
    ...or should I say ZEESH?
    
    ...I'll see myself out.

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ set -e
 cp -r dist .git
 git checkout gh-pages
 cp -r .git/dist/* .
-gc -m "Autocommit $(date +%Y-%m-%d:%H:%M:%S)"
+git commit -m "Autocommit $(date +%Y-%m-%d:%H:%M:%S)"
 git checkout master
